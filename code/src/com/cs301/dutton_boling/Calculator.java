7977874f3f5949c0dcf5fa8d110e3fa5ed3c0427
Stewart Boling <sjbkcc@mst.edu>
2013-10-20 23:29:51 -0500

    Started Calculator and added TODO items

--- code/src/com/cs301/dutton_boling/Calculator.java
+++ code/src/com/cs301/dutton_boling/Calculator.java
@@ -1,5 +1,10 @@
 package com.cs301.dutton_boling;
 
+import com.cs301.dutton_boling.models.Entry;
+import com.cs301.dutton_boling.models.Rule;
+
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Stewart
@@ -8,4 +13,16 @@ package com.cs301.dutton_boling;
  * To change this template use File | Settings | File Templates.
  */
 public class Calculator {
+
+    public List<Rule> calculateRules(List<Entry> entries){
+        List<List<Entry>> coverings = calculateCoverings(entries);
+        //TODO Iterate through coverings and calculate rules
+
+        return null;
+    }
+
+    private List<List<Entry>> calculateCoverings(List<Entry> entries){
+        //TODO This
+        return null;
+    }
 }
